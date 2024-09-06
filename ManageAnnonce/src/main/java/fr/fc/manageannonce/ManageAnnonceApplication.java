c74053e25726503f9724ea053aa6135abdbd63d7
Fares Boussoum <faresboussoum96@gmail.com>
2024-09-06 10:42:46 +0200

    initialisation

---
+++ ManageAnnonce/src/main/java/fr/fc/manageannonce/ManageAnnonceApplication.java
@@ -0,0 +1,13 @@
+package fr.fc.manageannonce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageAnnonceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManageAnnonceApplication.class, args);
+    }
+
+}
---
+++ ManageAnnonce/src/test/java/fr/fc/manageannonce/ManageAnnonceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.fc.manageannonce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageAnnonceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
